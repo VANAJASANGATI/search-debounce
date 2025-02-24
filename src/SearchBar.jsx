424e868cd702d5cea81adc236a5b0af18bfa4d42
S Vanaja <93426943+VANAJASANGATI@users.noreply.github.com>
2025-02-24 05:21:45 +0000

    search bar setshow added

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -2,10 +2,12 @@ import { useState, useEffect } from "react";
 export const SearchBar = () => {
   const [results, setResults] = useState([]);
   const [searchInput, setSearchInput] = useState("");
-  const [cache, setCache] = useState({});
+  const [cache, setCache] = useState([]);
+  const [show, setShow] = useState(false);
   useEffect(() => {
     if (cache[searchInput]) {
-      setResults((prev)=>);
+      setResults(cache);
+      return;
     }
     const timeId = setTimeout(() => {
       console.log("debiunce");
@@ -22,21 +24,31 @@ export const SearchBar = () => {
     );
     const data = await reciepiesJsonData.json();
     setResults(data.recipes);
-    setCache((prev) => ({ ...prev, [searchInput]: data }));
+    setCache((prev) => [...prev, { [searchInput]: data }]);
   }
 
   return (
-    <div style={{ width: "100%" }}>
+    <div style={{ width: "70%" }}>
       <input
-        style={{ width: "100%" }}
+        style={{ width: "70%" }}
         value={searchInput}
         onChange={(e) => setSearchInput(e.target.value)}
+        onFocus={() => setShow(true)}
+        onBlur={() => setShow(false)}
       />
-      <div style={{ border: "1px solid black", padding: 10, width: "100%" }}>
-        {results.map((item) => (
-          <p key={item.id}>{item.name}</p>
-        ))}
-      </div>
+      {show && (
+        <div
+          style={{
+            border: "1px solid black",
+            padding: 10,
+            width: "100%",
+          }}
+        >
+          {results.map((item) => (
+            <p key={item.id}>{item.name}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
