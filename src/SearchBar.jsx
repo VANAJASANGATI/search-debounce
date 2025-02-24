00b7f82d716ccf22fbae6890b46c31076bc5b900
S Vanaja <93426943+VANAJASANGATI@users.noreply.github.com>
2025-02-24 04:25:01 +0000

    search bar ui

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
 export const SearchBar = () => {
+  const [results, setResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  useEffect(() => {
+    fetchData();
+  }, [searchInput]);
+  async function fetchData() {
+    const reciepiesJsonData = await fetch(
+      `https://dummyjson.com/recipes/search?q=+${searchInput}`
+    );
+    const data = await reciepiesJsonData.json();
+    console.log("data", data);
+  }
+  //fetchData();
+  console.log("searchInput", searchInput);
+
   return (
     <div>
-      <input />
+      <input
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
     </div>
   );
 };
