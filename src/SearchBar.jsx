1c30a2829562a72faeb3607ee1e05e7e3a7e61a6
S Vanaja <93426943+VANAJASANGATI@users.noreply.github.com>
2025-02-24 04:55:15 +0000

    searchinput changes

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -2,25 +2,41 @@ import { useState, useEffect } from "react";
 export const SearchBar = () => {
   const [results, setResults] = useState([]);
   const [searchInput, setSearchInput] = useState("");
+  const [cache, setCache] = useState({});
   useEffect(() => {
-    fetchData();
+    if (cache[searchInput]) {
+      setResults((prev)=>);
+    }
+    const timeId = setTimeout(() => {
+      console.log("debiunce");
+      fetchData();
+    }, 500);
+    return () => {
+      clearTimeout(timeId);
+    };
   }, [searchInput]);
+
   async function fetchData() {
     const reciepiesJsonData = await fetch(
       `https://dummyjson.com/recipes/search?q=+${searchInput}`
     );
     const data = await reciepiesJsonData.json();
-    console.log("data", data);
+    setResults(data.recipes);
+    setCache((prev) => ({ ...prev, [searchInput]: data }));
   }
-  //fetchData();
-  console.log("searchInput", searchInput);
 
   return (
-    <div>
+    <div style={{ width: "100%" }}>
       <input
+        style={{ width: "100%" }}
         value={searchInput}
         onChange={(e) => setSearchInput(e.target.value)}
       />
+      <div style={{ border: "1px solid black", padding: 10, width: "100%" }}>
+        {results.map((item) => (
+          <p key={item.id}>{item.name}</p>
+        ))}
+      </div>
     </div>
   );
 };
